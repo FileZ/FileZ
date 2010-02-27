db881f486c44d6975160a90ab426579c6d0aa181
Arnaud Didry <arnaud.didry@univ-avignon.fr>
2010-02-27 14:46:05 +0000

    Log Zend_Mail_Exception when uploading files

--- app/controllers/Upload.php
+++ app/controllers/Upload.php
@@ -24,9 +24,11 @@ class App_Controller_Upload extends Fz_Controller {
             $jsonData['statusText'] = __('The file was successfuly uploaded');
             $jsonData['html']       = partial ('main/_file_row.php', array ('file' => $file));
 
-            try { $this->sendFileUploadedMail ($file); }
-            catch {
-                // Log "Can't send email Uploaded"
+            try { 
+                $this->sendFileUploadedMail ($file);
+            }
+            catch (Exception $e) {
+                fz_log ('Can\'t send email Uploaded', FZ_LOG_ERROR);
             }
 
         } else { // Errors happened while moving the uploaded file
