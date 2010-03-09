19b7da506280c10efa8641192dc3ecd4db2dc9f8
Arnaud Didry <arnaud.didry@univ-avignon.fr>
2010-03-09 15:02:31 +0000

    Updated Cron job

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
+# -----------------------------------------------------------------------------
+# Cron job used to delete expired files.
+#
+# This script needs to be executed by www user in order to append data to
+# existing log files.
+# -----------------------------------------------------------------------------
 
-# Directory where log files will be stored
-DIR=/var/log/filez
+# Directory where log file will be stored. Could be the same as in filez.ini
+# In this case, messages will be appended at the end of the file
+LOG_DIR=/var/log/filez
 
-# File containing URL output
-FILE=filez-cron-`date +"%Y%m%d"`.log
+# Url of the CRON web task
+URL=http://filez-url/admin/checkFiles
 
-# File containing wget errors
-LOGFILE=wget.log
+url_output="`wget -q -O - $URL`"
 
-# Url of the CRON task
-URL=http://localhost/fz/admin/checkFiles
-
-cd $DIR
-wget $URL -O $FILE -o $LOGFILE
-
+if test -n "$url_output"; then
+    cd $LOG_DIR
+    echo "[`date +'%FT%T%:z'`] [CRON] wget output: "$url_output >> "$LOG_DIR/filez-cron-error.log"
+fi
