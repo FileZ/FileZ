e7d3a3fc5c4fb5b7ef71215b369c6794209704c7
Arnaud Didry <arnaud.didry@univ-avignon.fr>
2010-03-25 00:00:38 +0000

    Updated Copying and Licence information

--- lib/Fz/Db/Table/Row/Abstract.php
+++ lib/Fz/Db/Table/Row/Abstract.php
@@ -1,9 +1,26 @@
 <?php
-
-
+/**
+ * Copyright 2010  Université d'Avignon et des Pays de Vaucluse 
+ * email: gpl@univ-avignon.fr
+ *
+ * This file is part of Filez.
+ *
+ * Filez is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Filez is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Filez.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /**
- * fzTableRow
+ * Fz_Db_Table_Row_Abstract
  */
 abstract class Fz_Db_Table_Row_Abstract {
 
--- resources/js/filez.js
+++ resources/js/filez.js
@@ -1,3 +1,23 @@
+/**
+ * Copyright 2010  Université d'Avignon et des Pays de Vaucluse 
+ * email: gpl@univ-avignon.fr
+ *
+ * This file is part of Filez.
+ *
+ * Filez is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Filez is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Filez.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 if (! console) // In case the browser don't have a console
     var console = {log: function (txt) {}};
 
