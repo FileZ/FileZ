a75a904d8c79f3636bc7bb5fe7f1ba564054f9cc
Arnaud Didry <arnaud@didry.info>
2011-03-09 23:38:53 +0100

    Fixed boolean handling when rendering filez.ini

--- lib/fz_config.php
+++ lib/fz_config.php
@@ -57,7 +57,7 @@ function merge_config ($user, $default) {
         $result [$section] = (array_key_exists ($section, $user) ?
             array_merge ($default[$section], $user[$section]) : $default[$section]);
     }
-    return $result + array_diff_key ($user, $default);
+    return $result;
 }
 
 /**
@@ -89,37 +89,23 @@ function fz_config_save ($config, $file) {
     return file_put_contents ($file, fz_serialize_ini_array($config, true));
 }
 
-function fz_serialize_ini_array ($assoc_arr, $has_sections=FALSE) {
+function fz_serialize_ini_array ($assoc_arr, $has_sections = false) {
     $content = "";
 
     if ($has_sections) {
-        foreach ($assoc_arr as $key=>$elem) {
-            $content .= "\n[".$key."]\n";
-            foreach ($elem as $key2=>$elem2)
-            {
-                if(is_array($elem2))
-                {
-                    for($i=0;$i<count($elem2);$i++)
-                    {
-                        $content .= $key2."[] = \"".$elem2[$i]."\"\n";
-                    }
-                }
-                else if($elem2 != "")
-                    $content .= $key2." = \"".$elem2."\"\n";
-            }
+        foreach ($assoc_arr as $section => $values) {
+            $content .= "\n[$section]\n".fz_serialize_ini_array($values);
         }
     }
     else {
-        foreach ($assoc_arr as $key=>$elem) {
-            if(is_array($elem))
-            {
-                for($i=0;$i<count($elem);$i++)
-                {
-                    $content .= $key2."[] = \"".$elem[$i]."\"\n";
-                }
-            }
-            else if($elem != "")
-                $content .= $key2." = \"".$elem."\"\n";
+        foreach ($assoc_arr as $key => $value) {
+            if (is_array ($value))
+                foreach ($value as $v)
+                    $content .= "{$key}[] = \"$v\"\n";
+            if (is_numeric ($value))
+                $content .= "$key = $value\n";
+            else if($value != "")
+                $content .= "$key = \"$value\"\n";
         }
     }
 
