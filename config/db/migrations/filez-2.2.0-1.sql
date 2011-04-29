c862cbc88c62e6fb320265101a94b8d67c0ad752
Arnaud Didry <arnaud.didry@gmail.com>
2011-04-29 01:08:11 -0700

    Fixed users migration

--- config/db/migrations/filez-2.2.0-1.sql
+++ config/db/migrations/filez-2.2.0-1.sql
@@ -12,11 +12,17 @@ CREATE TABLE `fz_user` (
   `created_at`  TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE = MYISAM ;
 
-ALTER TABLE `fz_file` DROP COLUMN `uploader_uid`;
-ALTER TABLE `fz_file` DROP COLUMN `uploader_email`;
+INSERT INTO fz_user (username, email) (SELECT DISTINCT  `uploader_uid` ,  `uploader_email` FROM  `fz_file` );
 
 ALTER TABLE `fz_file` ADD COLUMN       `created_by` INTEGER NOT NULL;
 ALTER TABLE `fz_file` ADD INDEX       (`created_by`);
 ALTER TABLE `fz_file` ADD FOREIGN KEY (`created_by`) REFERENCES fz_user(id);
+
+UPDATE `fz_file`, `fz_user` 
+SET `created_by` =`fz_user`.`id`
+WHERE `uploader_uid` = `username`;
+
+ALTER TABLE `fz_file` DROP COLUMN `uploader_uid`;
+ALTER TABLE `fz_file` DROP COLUMN `uploader_email`;
 
 UPDATE `fz_info` SET `value`='2.2.0-1' WHERE `key`='db_version';
