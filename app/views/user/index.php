cd63dc17aa3bfd8a954a7f8bcf0b76cef80dd988
Arnaud Didry <arnaud@didry.info>
2011-05-03 01:52:53 +0200

    Fix message escaping when asking for delete confirmation

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -16,14 +16,14 @@
 
 <?php foreach ($users as $user_item): ?>
   <tr>
-    <td><a href="<?php echo url_for ('/admin/users/'.$user_item->id) ?>"><?php echo $user_item." (".$user_item->username.")" ?></a></td>
+    <td><a href="<?php echo url_for ('/admin/users/'.$user_item->id) ?>"><?php echo h($user_item)." (".h($user_item->username).")" ?></a></td>
     <td><?php echo ($user_item->is_admin) ? __('admin') : '-' ?></td>
     <td><?php echo count ($user_item->getFiles ()) ?></td>
     <td><?php echo '125Mo' /* TODO */ ?></td>
     <td><?php echo '15' /* TODO */ ?></td>
-    <?php if ( $fz_user->id != $user_item->id ) : ?>
+    <?php if ( $fz_user->id != $user_item->id ) : // prevents self-deleting ?>
       <td>
-        <a onclick="javascript:return confirm ('<?php echo __r('Are you sure you want to delete the user "%displayname%" (%username%)', array ('displayname' => $user_item, 'username' => $user_item->username)) ?>')"
+        <a onclick='javascript:return confirm (<?php echo json_encode( __r('Are you sure you want to delete the user "%displayname%" (%username%)', array ('displayname' => $user_item, 'username' => $user_item->username))) ?>)'
            href="<?php echo url_for ('/admin/users/'.$user_item->id.'/delete') ?>">
           <?php echo __('Delete') ?>
         </a>
