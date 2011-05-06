dd65ed0d577c08482dbf1d7802ee1157adf21479
Alfred Maghi <w@larri.eu>
2011-05-06 14:48:48 +0000

    start the adoption of code documentation guidelines.

--- app/models/DbTable/Invitation.php
+++ app/models/DbTable/Invitation.php
@@ -9,24 +9,16 @@
  * @package FileZ
  */
 
-class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
+class App_Model_DbTable_Invitation extends Fz_Db_Table_Abstract {
 
-    protected $_rowClass = 'App_Model_File';
-    protected $_name = 'fz_file';
+    protected $_rowClass = 'App_Model_Invitation';
+    protected $_name = 'fz_invitation';
     protected $_columns = array (
-        'del_notif_sent',
-        'file_name',
-        'file_size',
-        'nom_physique',
+        'invitation_name',
         'available_from',
         'available_until',
-        'download_count',
-        'notify_uploader',
         'created_by',
         'created_at',
-        'extends_count',
-        'comment',
-        'password',
     );
 
     /**
@@ -67,7 +59,7 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
     }
 
     /**
-     * Return a free slot id in the fz_file table
+     * Return a free slot id in the fz_invitation table
      * 
      * @return integer
      */
@@ -82,20 +74,20 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
     }
 
     /**
-     * Find a file by its hash code
+     * Find a invitation by its hash code
      * 
      * @param string $hash
-     * @return App_Model_File
+     * @return App_Model_Invitation
      */
     public function findByHash ($hash) {
         return $this->findById ($this->hashToId ($hash));
     }
 
     /**
-     * Find a file by its hash code (Filez 1.x only)
+     * Find a invitation by its hash code (Invitationz 1.x only)
      *
      * @param string $hash
-     * @return App_Model_File
+     * @return App_Model_Invitation
      */
     public function findByFzOneHash ($hash) {
         $sql = 'SELECT * FROM '.$this->getTableName ().' WHERE adresse = ?';
@@ -103,10 +95,10 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
     }
 
     /**
-     * Return all file owned by $uid which are available (not deleted)
+     * Return all invitation owned by $uid which are available (not deleted)
      *
      * @param App_Model_User $user
-     * @return array of App_Model_File
+     * @return array of App_Model_Invitation
      */
     public function findByOwnerOrderByUploadDateDesc ($user) {
         $sql = 'SELECT * FROM '.$this->getTableName ()
@@ -117,17 +109,17 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
     }
 
     /**
-     * Delete files whose lifetime expired
+     * Delete invitations whose lifetime expired
      */
-    public function deleteExpiredFiles () {
+    public function deleteExpiredInvitations () {
         $select = 'SELECT * FROM '.$this->getTableName ();
         $where  = ' WHERE available_until<CURRENT_DATE()';
-        foreach ($this->findBySql ($select.$where) as $file) {
-            if ($file->deleteFromDisk () === true) {
-                fz_log ('Deleted file "'.$file->getOnDiskLocation ().'"',
+        foreach ($this->findBySql ($select.$where) as $invitation) {
+            if ($invitation->deleteFromDisk () === true) {
+                fz_log ('Deleted invitation "'.$file->getOnDiskLocation ().'"',
                         FZ_LOG_CRON);
             } else {
-                fz_log ('Failed deleting file "'.$file->getOnDiskLocation ().'"',
+                fz_log ('Failed deleting invitation "'.$file->getOnDiskLocation ().'"',
                         FZ_LOG_CRON_ERROR);
             }
         }
@@ -135,13 +127,13 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
     }
 
     /**
-     * Return files which will be deleted within X days and where uploader wants
+     * Return invitations which will be deleted within X days and where uploader wants
      * to be notified but hasn't been yet
      * 
      * @param integer   $days   Number of days before being deleted
-     * @return App_Model_File
+     * @return App_Model_Invitation
      */
-    public function findFilesToBeDeleted ($days = 2) {
+    public function findInvitationsToBeDeleted ($days = 2) {
         $sql = 'SELECT * FROM '.$this->getTableName ()
               .' WHERE available_until BETWEEN CURRENT_DATE() '
               .'AND DATE_ADD(CURRENT_DATE(), INTERVAL '.$days.' DAY) '
@@ -158,24 +150,13 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
      */
     public function getTotalDiskSpaceByUser ($user) {
         $result = option ('db_conn')
-            ->prepare ('SELECT sum(file_size) FROM `'
+            ->prepare ('SELECT sum(invitation_size) FROM `'
                 .$this->getTableName ()
                 .'` WHERE created_by = ?'
                 .' AND  available_until >= CURRENT_DATE() ');
         $result->execute (array ($user->id));
         return (float) $result->fetchColumn ();
     }
-
-    /**
-     * Return readable disk space used by someone
-     *
-     * @param App_Model_User    $user   User
-     * @return string           Readable size
-     */
-    public function getReadableTotalDiskSpaceByUser ($user) {
-        return $this->getReadableSize ($this->getTotalDiskSpaceByUser ($user));
-    }
-
 
     /**
      * Return remaining disk space available for user $user
@@ -203,45 +184,6 @@ class App_Model_DbTable_File extends Fz_Db_Table_Abstract {
         }
         return floatval ($size);
     }
-
-    /**
-     * Count the number of files
-     * 
-     * @return integer number of files
-     */
-    public function getNumberOfFiles () {
-        $sql = 'SELECT COUNT(*) AS count FROM '.$this->getTableName ();
-        $res = Fz_Db::findAssocBySQL($sql);
-        return $res[0]['count'];
-    }
-
-    /**
-     * Return disk space used by everybody
-     *
-     * @return float  Size in bytes
-     */
-    public function getTotalDiskSpace () {
-        $result = option ('db_conn')
-            ->prepare ('SELECT sum(file_size) FROM `'
-                .$this->getTableName ()
-                .'` WHERE available_until >= CURRENT_DATE() ');
-        $result->execute ();
-        return $this->getReadableSize ($result->fetchColumn ());
-    }
-
-    /**
-     * Return bytes size to be read by human
-     *
-     * @param float $bytes
-     * @return string
-     */
-    public function getReadableSize ($bytes, $precision = 2) {
-        $units = array(__('B'), __('KB'), __('MB'), __('GB'), __('TB'));
-        $pow = floor (($bytes ? log($bytes) : 0) / log(1024));
-        $pow = min($pow, count($units) - 1);
-
-        $bytes /= pow(1024, $pow);
-
-        return round($bytes, $precision) . ' ' . $units[$pow];
-    }
 }
+
+
