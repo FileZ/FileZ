41aaf29018908338f752d527f7b277d4c2a37b68
Alfred Maghi <w@larri.eu>
2011-05-06 09:15:22 +0000

    cross-site security with the h() function

--- app/views/user/show.php
+++ app/views/user/show.php
@@ -4,8 +4,8 @@
   </a>
 <?php endif ?></h2>
 
-<p><b><?php echo __('Email') ?> :</b> <?php echo $user->email ?></p>
-<p><b><?php echo __('Account created') ?> :</b> <?php echo $user->created_at ?></p>
+<p><b><?php echo __('Email') ?> :</b> <?php echo h($user->email) ?></p>
+<p><b><?php echo __('Account created') ?> :</b> <?php echo h($user->created_at) ?></p>
 <p><b><?php echo __('Administrator ?') ?> :</b> <?php echo $user->is_admin ? __('yes') : __('no') ?></p>
 
 <table id="user_files" class="data">
@@ -19,16 +19,16 @@
 
 <?php foreach ($user->getFiles () as $file): ?>
   <tr>
-    <td><a href="<?php echo $file->getDownloadUrl () ?>"><?php echo $file->file_name ?></a></td>
+    <td><a href="<?php echo h($file->getDownloadUrl ()) ?>"><?php echo h($file->file_name) ?></a></td>
     <td><?php echo __r('from %from% to %to%', array (
       'from' => ($file->getAvailableFrom  ()->get (Zend_Date::MONTH) ==
                  $file->getAvailableUntil ()->get (Zend_Date::MONTH)) ?
                  $file->getAvailableFrom ()->toString ('d') : $file->getAvailableFrom ()->toString ('d MMMM'),
       'to' =>  '<b>'.$file->getAvailableUntil ()->toString ('d MMMM').'</b>')) // FIXME I18N ?>
     </td>
-    <td><?php echo $file->getReadableFileSize () ?></td>
-    <td><?php echo (int) $file->download_count ?></td>
-    <td><a href="<?php echo $file->getDownloadUrl () . '/delete' ?>"><?php echo __('Delete') ?></a></td>
+    <td><?php echo h($file->getReadableFileSize ()) ?></td>
+    <td><?php echo (int) h($file->download_count) ?></td>
+    <td><a href="<?php echo h($file->getDownloadUrl ()) . '/delete' ?>"><?php echo __('Delete') ?></a></td>
   </tr>
 <?php endforeach ?>
 </table>
