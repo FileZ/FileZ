dcdcefcf61097181ce23d4d3d018506cb1378c39
William Larrieu <william@larri.eu>
2011-10-21 16:00:54 +0200

    admin settings: implode array in settings value

--- app/views/admin/config.php
+++ app/views/admin/config.php
@@ -1,9 +1,12 @@
 <h2><?php echo __('FileZ settings') ?></h2>
 
 <?php foreach ($config as $category=>$settings): ?>
+<?php if ($category!="db"): ?>
 	  <h3><?php echo $category; ?></h3>
 <?php foreach ($settings as $set=> $value): ?>
+<?php if (is_array($value)) $value = implode(", ", $value) ?>
 	    <li><?php echo $set." = ".$value; ?></li>
 <?php endforeach ?>
 	    <br>
+<?php endif ?>
 <?php endforeach ?>
