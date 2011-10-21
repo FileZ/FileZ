64cff0ab3fa4f894515e8faef303bc254c587e82
William Larrieu <william@larri.eu>
2011-10-21 16:25:30 +0200

    admin settings: add a notice to explain how to edit the settings

--- app/views/admin/config.php
+++ app/views/admin/config.php
@@ -1,5 +1,8 @@
 <h2><?php echo __('FileZ settings') ?></h2>
 
+<?php echo __('System administrators can edit the settings in the file config/filez.ini') ?>
+<br><br>
+
 <?php foreach ($config as $category=>$settings): ?>
 <?php if ($category!="db"): ?>
 	  <h3><?php echo $category; ?></h3>
