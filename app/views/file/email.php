0a8de6b59d45bf70dede44de88b8c65a31fc3d05
William Larrieu <w@larri.eu>
2012-03-03 15:29:02 +0100

    fix #64 by steamraven

--- app/views/file/email.php
+++ app/views/file/email.php
@@ -1,4 +1,4 @@
 
-<h2>Envoyer le fichier par email <span class="filename">(<?php echo h($file->file_name) ?>)</span></h2>
+<h2><?php echo __("Send via email:") ?> <span class="filename">(<?php echo h($file->file_name) ?>)</span></h2>
 
 <?php echo partial ('file/_mailForm.php', array ('file' => $file)) ?>
